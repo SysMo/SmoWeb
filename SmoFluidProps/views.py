73597fb837a58bff2c1ac60fe933086f7a62f439
Atanas Pavlov <atanas.pavlov@sysmoltd.com>
2014-09-15 15:10:24 +0300

    Temporary
    Signed-off-by: Atanas Pavlov <atanas.pavlov@sysmoltd.com>

--- SmoFluidProps/views.py
+++ SmoFluidProps/views.py
@@ -8,38 +8,23 @@ from django.core.context_processors import csrf
 class FluidProps_SetUpView(TemplateView):
 	template_name = "SmoFluidProps/SetUp.html"
 	def get(self, request, fluidPropsId = None):
-#		import uuid
-#		id = uuid.uuid1().int
 		instance = FluidProps_SetUpModel()
-#		FluidProps_ObjectList[id] = instance
 		form = FluidProps_SetUpForm(instance = instance)
 		context = self.get_context_data()
-#		context['id'] = id
 		context['form'] = form
 		context['locals'] = {}
 		return render(self.request, self.template_name, context)
 	def post(self, request):
 		form = FluidProps_SetUpForm(request.POST)
 		if form.is_valid():
-			print form.cleaned_data
+			pass
 		else:
 			raise ValueError("Invalid form data")
-
 		instance = FluidProps_SetUpModel(**form.cleaned_data)
-# 		for field in instance._meta.fields:
-# 			if (field.name != 'id'):
-# 				print(field.name)
-# 				print(field.name, request.POST[field.name])
-# 				instance.__setattr__(field.name, request.POST[field.name])
-
-		
 		columnNames, propsTable = instance.computeFluidProps()
-		
 		context = self.get_context_data()
 		context['form'] = form
 		context['locals'] = {'propsTable' : propsTable, 'columnNames' : columnNames}
-		#print propsTable
-
 		return render(self.request, self.template_name, context)		
 		
 	
