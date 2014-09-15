d571fdd514dc38a4fd2125dc011c721bf36478fd
Ivaylo Mihaylov <ivombg@yahoo.com>
2014-09-15 18:58:06 +0300

    Simplified SmoFluidProps views.py
    Signed-off-by: Ivaylo Mihaylov <ivombg@yahoo.com>

--- SmoFluidProps/views.py
+++ SmoFluidProps/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, RequestContext
 from django.views.generic import TemplateView
 from SmoFluidProps.forms import FluidProps_SetUpForm
 from SmoFluidProps.models import FluidProps_SetUpModel
@@ -7,25 +7,20 @@ from django.core.context_processors import csrf
 
 class FluidProps_SetUpView(TemplateView):
 	template_name = "SmoFluidProps/SetUp.html"
-	def get(self, request, fluidPropsId = None):
-		instance = FluidProps_SetUpModel()
-		form = FluidProps_SetUpForm(instance = instance)
-		context = self.get_context_data()
-		context['form'] = form
-		context['locals'] = {}
-		return render(self.request, self.template_name, context)
+	def get(self, request):
+		form = FluidProps_SetUpForm()
+		
+		return render_to_response(self.template_name, locals(), 
+								context_instance=RequestContext(self.request))
 	def post(self, request):
-		form = FluidProps_SetUpForm(request.POST)
-		if form.is_valid():
-			pass
-		else:
+		form = FluidProps_SetUpForm(self.request.POST)
+		if not form.is_valid():
 			raise ValueError("Invalid form data")
 		instance = FluidProps_SetUpModel(**form.cleaned_data)
 		columnNames, propsTable = instance.computeFluidProps()
-		context = self.get_context_data()
-		context['form'] = form
-		context['locals'] = {'propsTable' : propsTable, 'columnNames' : columnNames}
-		return render(self.request, self.template_name, context)		
+		
+		return render_to_response(self.template_name, locals(), 
+								context_instance=RequestContext(self.request))		
 		
 	
 	
