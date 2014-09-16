175f52a81bbf2129f70762da08c184a1c595b512
Ivaylo Mihaylov <ivombg@yahoo.com>
2014-09-16 17:24:30 +0300

    Created SmoWeb navigation.
    Signed-off-by: Ivaylo Mihaylov <ivombg@yahoo.com>

--- SmoWeb/urls.py
+++ SmoWeb/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SmoWeb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'SmoWeb.views.home', name='home' ),
+    url(r'^platform/', 'SmoWeb.views.platform', name='platform'),
     url(r'^SmoFluidProps/', include('SmoFluidProps.urls', namespace = 'SmoFluidProps')),
     url(r'^admin/', include(admin.site.urls)),
 )
