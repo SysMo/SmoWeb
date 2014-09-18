95414ff29c0a30bb68f9f7355b092e12e31a113b
Ivaylo Mihaylov <ivombg@yahoo.com>
2014-09-18 15:22:11 +0300

    Added pop-up alert for invalid file upload form input.
    Signed-off-by: Ivaylo Mihaylov <ivombg@yahoo.com>

--- DataManagement/views.py
+++ DataManagement/views.py
@@ -12,11 +12,15 @@ def importCSV(request):
         if form.is_valid():
             handle_uploaded_file(form.cleaned_data['csvFile'], 
                                  os.path.join(MEDIA_ROOT, 'DataManagement', 'csv'))
+            errMessage = ""
         else:
-            raise ValueError("Invalid form data")
+            errMessage = "Invalid form data"
+        
         #print request.FILES
         #print form.cleaned_data['csvFile']
         #print form.cleaned_data['hdfPath']
                                 
-    form = ImportCSV_Form()
+    else:
+        form = ImportCSV_Form()
+        errMessage = ""
     return render_to_response('CSVform.html', locals(), context_instance=RequestContext(request))
