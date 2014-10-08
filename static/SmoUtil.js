7b8c2080f67bb68b0db379c855de343a717a80d1
Atanas Pavlov <atanas.pavlov@sysmoltd.com>
2014-10-08 19:17:45 +0300

    Created Quantity class in JS and defined some standard quantities.
    Signed-off-by: Atanas Pavlov <atanas.pavlov@sysmoltd.com>

--- static/SmoUtil.js
+++ static/SmoUtil.js
@@ -1,7 +1,7 @@
 smoModule = angular.module('smo', []);
 
 smoModule.factory('util', function util () {
-	functions = {};
+	var functions = {};
 	function dumpObject(obj, indent) {
 		var result = "";
 		if (indent == null)
@@ -57,7 +57,74 @@ smoModule.directive('smoSingleClick', ['$parse', function($parse) {
     };
 }]);
 
-smoModule.directive('smoResizer', function($document) {
+smoModule.factory('units', function() {
+	var units = {};
+	// List of quantities and units
+	units.quantities = {
+		'Length' : {defUnit : 'm', units : {'m' : {mult : 1}, 'km' : {mult : 1e3}, 'cm' : {mult : 1e-2}, 'mm' : {mult : 1e-3}, 
+			'um' : {mult : 1e-6}, 'nm' : {mult : 1e-9}, 'in' : {mult : 2.54e-2}, 'ft' : {mult : 3.048e-1}}},
+		'Area' : {defUnit : 'm**2', units : {'m**2' : {mult : 1}, 'cm**2' : {mult : 1e-4}, 'mm**2' : {mult : 1e-6}}}, 
+		'Volume' : {defUnit : 'm**3', units : {'m**3' : {mult : 1}, 'cm**3' : {mult : 1e-6}, 'mm**3' : {mult : 1e-9}}},
+		'Time' : {defUnit : 's', units : {'s' : {mult : 1}, 'ms' : {mult : 1e-3}, 'us' : {mult : 1e-6}, 'min' : {mult : 60}, 'h' : {mult : 3600}, 'day' : {mult : 8.64e4}, 'year' : {mult : 3.15576e7}}}, 
+		'Mass' : {defUnit : 'kg', units : {'kg' : {mult : 1}, 'ton' : {mult : 1e3}, 'g' : {mult : 1e-3}}},
+		'Pressure' : {defUnit : 'bar', units : {'Pa' : {mult : 1}, 'kPa' : {mult : 1e3}, 'MPa' : {mult : 1e6}, 'GPa' : {mult : 1e9}, 
+				'bar' : {mult : 1e5}, 'psi' : {mult : 6.89475e3}, 'ksi' : {mult : 6.89475e6}}},
+		'Temperature' : {defUnit : 'K', units : {'K' : {mult : 1}, 'degC' : {mult : 1, offset : 273.15}, 'degF' : {mult : 5./9, offset : 255.372}}},
+		'Density' : {defUnit : 'kg/m**3', units : {'kg/m**3' : {mult : 1}, 'g/L' : {mult : 1}, 'g/cm**3' : {mult : 1e3}}},
+		'SpecificEnthalpy' : {defUnit : 'kJ/kg', units : {'J/kg' : {mult : 1}, 'kJ/kg' : {mult : 1e3}}},
+		'SpecificInternalEnergy' : {defUnit : 'kJ/kg', units : {'J/kg' : {mult : 1}, 'kJ/kg' : {mult : 1e3}}},
+		'SpecificEntropy' : {defUnit : 'kJ/kg-K', units : {'J/kg-K' : {mult : 1}, 'kJ/kg-K' : {mult : 1e3}}},
+		'VaporQuality' : {defUnit : '-', units : {'-' : {mult : 1}}}
+	};
+
+	// Object for handling quantity
+	var Quantity = function (quantity, value, unit, displayUnit) {		
+		this.quantity = quantity;
+		unit = unit || units.quantities[this.quantity].defUnit;
+		this.displayUnit = displayUnit || unit; //units.quantities[this.quantity].defUnit;
+		
+		var unitDef = units.quantities[this.quantity].units[unit];		
+		var offset = unitDef.offset || 0;
+		
+		this.value = value * unitDef.mult + offset;		
+		this.changeUnit();
+	}
+
+	Quantity.prototype.updateQuantity = function() {
+		var dispUnitDef = units.quantities[this.quantity].units[this.displayUnit];
+		var offset = 0;
+		if ('offset' in dispUnitDef) {
+			offset = dispUnitDef.offset;
+		}
+		this.value = this.displayValue * dispUnitDef.mult + offset ; 
+	}
+	Quantity.prototype.changeUnit = function() {
+		var dispUnitDef = units.quantities[this.quantity].units[this.displayUnit];
+		var offset = 0;
+		if ('offset' in dispUnitDef) {
+			offset = dispUnitDef.offset;
+		}
+		this.displayValue = (this.value - offset) / dispUnitDef.mult; 
+	}
+	units.Quantity = Quantity;
+	return units;
+});
+
+smoModule.directive('smoInputQuantity', ['$compile', function($compile) {
+	return {
+		restrict : 'E',
+		link : function(scope, element, attr) {
+			var qVar = attr.smoQuantityVar;
+			var template =  '<input type="number" step="any" ng-init="' + qVar + '.changeUnit()" ng-model="' + qVar + '.displayValue" ng-change="' + qVar + '.updateQuantity()">' +
+			'<select ng-model="' + qVar + '.displayUnit" ng-options="name as name for (name, conv) in units.quantities[' + qVar + '.quantity].units" ng-change="' + qVar + '.changeUnit()"></select>';
+			element.html('').append($compile(template)(scope));
+		}
+	}
+}]);
+
+/*
+ * Some code that could be used for resizing but doesn't work currently
+ smoModule.directive('smoResizer', function($document) {
 
 	return function($scope, $element, $attrs) {
 
@@ -112,3 +179,4 @@ smoModule.directive('smoResizer', function($document) {
 		}
 	};
 });
+*/
