9dd2a3953bc5c4013188f842172ec3a8b924b438
Atanas Pavlov <atanas.pavlov@sysmoltd.com>
2014-11-30 18:04:55 +0200

    Added tasks to the Fabric file for installing apt and pip packages to the server.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,9 @@
-# Call with
-# fab -H localhost deploy
-
 import os
 import glob
-from fabric.api import *
+from fabric.api import run, sudo, env, cd, prefix, local, hosts
 from contextlib import contextmanager as _contextmanager
+from fabric.contrib import files
+from fabric.context_managers import shell_env
 
 srvAddress = 'platform.sysmoltd.com' 
 
@@ -22,20 +21,30 @@ env.extraFolders = [
 	'smo',
 	'templates'
 ]
-
 env.folderCopyList = [
 	'manage.py',
 ] + [env.projectModule] \
   + env.applicationModules \
   + env.extraFolders
 
+env.tempFolder = os.path.abspath(os.path.join(os.path.expanduser('~'), 'tmp'))
+
 @_contextmanager
 def virtualenv():
-	with cd(env.projectRoot):
-		with prefix(env.activate):
-			yield
+	with prefix(env.activate):
+		yield
 
+#######################################################################
+# Synchronize the local and remote virtual environments
+#######################################################################
+def syncVirtEnv():
+	local('unison -ignore "Name {*.pyc}" /srv/VirtualEnv/ ssh://' + srvAddress + '//srv/VirtualEnv')
+	sudo('chown -R www-data:www-data /srv/SmoWeb ')
+	sudo('service apache2 restart')
 
+#######################################################################
+# Deploy the new Django version to the server
+#######################################################################
 def deploy():
 	print("Cleaning up old code and static files")
 	local('rm -rf {0}/*'.format(os.path.join(env.installDir, 'Static')))
@@ -50,11 +59,10 @@ def deploy():
 	sudo('chown -R www-data:www-data /srv/SmoWeb ')
 	sudo('service apache2 restart')
 		
-def syncVirtEnv():
-	local('unison -ignore "Name {*.pyc}" /srv/VirtualEnv/ ssh://' + srvAddress + '//srv/VirtualEnv')
-	sudo('chown -R www-data:www-data /srv/SmoWeb ')
-	sudo('service apache2 restart')
 	
+#######################################################################
+# Generate HTML files from restructuredText documents
+#######################################################################
 def rest2html():
 	from docutils.core import publish_parts
 	from docutils import io
@@ -76,4 +84,95 @@ def rest2html():
 				with codecs.open(outputFilePath, 'w', 'utf-8') as fOut:
 					fOut.write(result['html_body'])
 				print ('Wrote output file: ' + outputFilePath)
+
+#######################################################################
+# Installing all the necessary apt packages on the server
+#######################################################################
+def installAptPackages():
+	packageList = [
+		# Revision control system
+		'git',
+		# Header files for compiling python modules
+		'python-dev',
+		# Fortran compiler
+		'gfortran',
+		# ATLAS linear algebra library
+		'libatlas-dev',
+		'libatlas-base-dev',
+		# MPI libraries
+		'mpi-default-dev',
+		# Parallel version of HDF5
+		'libhdf5-openmpi-7',
+		# Sparse solvers
+		'libsuitesparse-dev',
+		# Apache web server
+		'apache2',
+		# Mod wsgi for running python web applications (necessary for Django)
+		'libapache2-mod-wsgi',
+		# Font library, necessary for matplotlib
+		'libfreetype6-dev',
+		# PNG read/write; necessary for matplotlib
+		'libpng-dev',
+		# necessary for matplotlib
+		'libqhull-dev',
+		'pkg-config',		
+	]
+	packageString = (" ").join(packageList)
+	sudo('apt-get install {0}'.format(packageString))
+
+
+#######################################################################
+# Installing the necessary python virtualenv packages on the server
+#######################################################################
+def installPipPackages():
+	with virtualenv():
+		packageList = [
+			'argparse',
+			'six',
+			'python-dateutil',
+			# Python wrappers for C/C++ 
+			'cython',
+			# Numerical libraries
+			'numpy',
+			'scipy',
+			# Matplotlib pre-requisites
+			'pytz',
+			'tornado',
+			'pyparsing',
+			# Finite volume solver
+			'ez_setup',
+			'fipy',
+			# Application server 
+			'django',
+			'django-bootstrap-toolkit',
+			'django-tastypie',
+			'wsgiref',
+			# System administration/deployment
+			'fabric',
+			
+		]
+		for package in packageList:
+			run('pip install {0}'.format(package))
+		
+#@hosts('localhost')
+def installPySparse():
+	with virtualenv(), cd(env.tempFolder):
+		#if (not os.path.isdir(os.path.join(env.tempFolder, 'pysparse'))):
+		if (not files.exists('pysparse')):
+			run('git clone git://pysparse.git.sourceforge.net/gitroot/pysparse/pysparse')
+		with cd('pysparse'):
+			run('python setup.py build')
+			run('python setup.py install')
+		
+def installMatplotlib():
+	""" Plotting library"""
+	with virtualenv():
+		with shell_env(CFLAGS="-I/usr/include/freetype2"):
+			run('pip install matplotlib')
+	
+	
+def installHdf():
+	with virtualenv():
+		with shell_env(CFLAGS="-I/usr/lib/openmpi/include/"):
+			run('pip install h5py')
 		
--- smo/simple_flow/FreeConvection.py
+++ smo/simple_flow/FreeConvection.py
@@ -5,79 +5,83 @@ Created on Nov 27, 2014
 '''
 from smo.numerical_model.model import NumericalModel
 from smo.numerical_model.fields import *
-import numpy
+import numpy as np
 from smo.smoflow3d.SimpleMaterials import Fluids
 from smo.smoflow3d.CoolProp.CoolProp import Fluid, FluidState
 from collections import OrderedDict
 from django.contrib.messages.tests.urls import show
 
-ConvectionConfigurations = OrderedDict((
-	('Vert', 'vertical plane'),
-	('Incl', 'inclined plane'),
-	('HorizTop', 'horizontal plane top'),
-	('HorizBottom', 'horizontal plane bottom'),
-	('VertCyl', 'vertical cylinder'),
-	('HorizCyl', 'horizontal cylinder'),
-	('Sphere', 'sphere'),
+GeometryConfigurations = OrderedDict((
+	('VP', 'vertical plane'),
+	('IP', 'inclined plane'),
+	('HPT', 'horizontal plane top'),
+	('HPB', 'horizontal plane bottom'),
+	('VC', 'vertical cylinder'),
+	('HC', 'horizontal cylinder'),
+	('SPH', 'sphere'),
 #	('Fin', 'finnedPipe'),
-	('ConvCoeff', 'convection coefficient')
+	('CCF', 'convection coefficient')
+	))
+
+PropTemperatureConf = OrderedDict((
+	('MT', 'mean temperature'),
+	('FT', 'fluid temperature')
 	))
 
 class FreeConvection(NumericalModel):
 	fluidName = Choices(Fluids, default = 'Nitrogen', label = 'fluid')	
 	TFluid = Quantity('Temperature', default = (15, 'degC'), label = 'fluid temeprature')
 	TWall = Quantity('Temperature', default = (50, 'degC'), label = 'wall temeprature')
-	useTFilm = Boolean(default = 'True', label = 'use film temperature')
+	propT = Choices(PropTemperatureConf, label = 'compute props at')
 	pressure = Quantity('Pressure', default = (1, 'bar'), label = 'fluid pressure') 
-	conf = Choices(ConvectionConfigurations, default = 'Vert', label = 'configuration')
-	thermalInputs = FieldGroup([fluidName, TFluid, TWall, useTFilm, conf], label = 'Thermal') 
+	geomConf = Choices(GeometryConfigurations, default = 'VP', label = 'configuration')
+	thermalInputs = FieldGroup([fluidName, pressure, TFluid, TWall, propT, geomConf], label = 'Thermal') 
 	
 	width = Quantity('Length', default = (1, 'm'), label = 'width', 
-			show = '(self.conf == "Vert") || (self.conf == "Incl") || (self.conf == "HorizTop") || (self.conf == "HorizBottom")')
+			show = '(self.geomConf == "VP") || (self.geomConf == "IP") || (self.geomConf == "HPT") || (self.geomConf == "HPB")')
 	length = Quantity('Length', default = (1, 'm'), label = 'length', 
-			show = '(self.conf == "Incl") || (self.conf == "HorizTop") || (self.conf == "HorizBottom") || (self.conf == "HorizCyl")')
+			show = '(self.geomConf == "IP") || (self.geomConf == "HPT") || (self.geomConf == "HPB") || (self.geomConf == "HC")')
 	height = Quantity('Length', default = (1, 'm'), label = 'height', 
-			show = '(self.conf == "Vert") || (self.conf == "VertCyl")')
+			show = '(self.geomConf == "VP") || (self.geomConf == "VC")')
 	diameter = Quantity('Length', default = (5, 'mm'), label = 'diameter', 
-		show = '(self.conf == "VertCyl") || (self.conf == "HorizCyl") || (self.conf == "Sphere")')
+		show = '(self.geomConf == "VC") || (self.geomConf == "HC") || (self.geomConf == "SPH")')
 	angle = Quantity('Angle', default = (45, 'deg'), label = 'inclination',
-			show ='self.conf == "Incl"')
-	inputArea = Quantity('Area', default = (1, 'm**2'), label = 'surface area', show = 'self.conf == "ConvCoeff"')
-	input_h = Quantity('HeatTransferCoefficient', label = 'convection coefficient', show = 'self.conf == "ConvCoeff"')
+			show ='self.geomConf == "IP"')
+	inputArea = Quantity('Area', default = (1, 'm**2'), label = 'surface area', show = 'self.geomConf == "CCF"')
+	input_h = Quantity('HeatTransferCoefficient', label = 'convection coefficient', show = 'self.geomConf == "CCF"')
 	geometryInputs = FieldGroup([width, length, height, diameter,  angle, inputArea, input_h], label = 'Geometry')
 	
 	inputs = SuperGroup([thermalInputs, geometryInputs])
 	###############
-	deltaT = Quantity('TemperatureDifference', label = 'temperature difference')
 	Tfilm = Quantity('Temperature', default = (30, 'degC'), label = 'film temperature')
-	Gr = Quantity('Dimensionless', label = 'Grashof number')
-	Ra = Quantity('Dimensionless', label = 'Rayleigh number')
-	Nu = Quantity('Dimensionless', label = 'Nusselt number')
-	QDot = Quantity('HeatFlowRate', label = 'heat flow rate')
-	Pr = Quantity('Dimensionless', label = 'Prandtl number')
 	cond = Quantity('ThermalConductivity', label = 'thermal conductivity')
 	rho = Quantity('Density', label = 'density')
 	mu = Quantity('DynamicViscosity', label = 'dynamic viscosity')
-	h = Quantity('HeatTransferCoefficient', label = 'convection coefficient')
 	beta = Quantity('Dimensionless', label = 'beta')
-	thermalResults = FieldGroup([deltaT, Tfilm, Gr, Pr, Ra, Nu, QDot, h, cond, rho, mu, beta], label = 'Thermal')
-	
+	Pr = Quantity('Dimensionless', label = 'Prandtl number')
+	fluidProps = FieldGroup([Tfilm, rho, cond, mu, beta, Pr], label = "Fluid propeties")
+
+	deltaT = Quantity('TemperatureDifference', label = 'temperature difference')
+	Gr = Quantity('Dimensionless', label = 'Grashof number')
+	Ra = Quantity('Dimensionless', label = 'Rayleigh number')
+	Nu = Quantity('Dimensionless', label = 'Nusselt number')
+	h = Quantity('HeatTransferCoefficient', label = 'convection coefficient')
 	area = Quantity('Area', label = 'surface area')
-	geometryResults = FieldGroup([area], label = 'Geometry')
-	
-	results = SuperGroup([thermalResults, geometryResults])
+	QDot = Quantity('HeatFlowRate', label = 'heat flow rate')	
+	heatExchangeResults = FieldGroup([deltaT, Gr, Ra, Nu, h, area, QDot], label = 'Heat exchange')
+		
+	results = SuperGroup([fluidProps, heatExchangeResults])
 	
 	def compute(self):
 		self.deltaT = self.TWall - self.TFluid
-		self.Tfilm = (self.TWall + self.TFluid) / 2
-		if (self.useTFilm):
-			TConv = self.Tfilm
+		if (self.propT == 'MT'):
+			self.Tfilm = (self.TWall + self.TFluid) / 2
 		else:
-			TConv = self.TFluid
+			self.Tfilm = self.TFluid
 		
+		# Compute fluid properties
 		fState = FluidState(self.fluidName)
-		fState.update_Tp(TConv, self.pressure)
-		
+		fState.update_Tp(self.Tfilm, self.pressure)		
 		self.rho = fState.rho()
 		self.mu = fState.mu()
 		nu = self.mu / self.rho
@@ -85,22 +89,33 @@ class FreeConvection(NumericalModel):
 		self.Pr = fState.Pr()
 		self.cond = fState.cond()
 		
-		if (self.conf == 'Vert'):
+		# Compute geometry factors
+		if (self.geomConf == 'VP'):
 			s = self.height
+			self.area = self.width * self.height
+		elif (self.geomConf == 'HC'):
+			s = self.diameter
+			self.area = np.pi * self.diameter * self.length	
 		else:
-			pass
+			raise ValueError("Geometry configuration {0} not implemented".format(GeometryConfigurations[self.geomConf]))
+		
+		# Compute free convection dimensionless numbers
 		self.Gr = 9.81 * (s ** 3) * self.beta * self.deltaT / (nu ** 2)
 		self.Ra = self.Gr * self.Pr
 		
-		if (self.conf == 'Vert'):
+		# Use the appropriate empirical Nusselt correlation
+		if (self.geomConf == 'VP'):
 			fPr = (1 + (0.492 / self.Pr) ** (9.0 / 16)) ** (-16.0 / 9)
 			self.Nu = (0.825 + 0.387 * (self.Ra * fPr) ** (1.0 / 6)) ** 2			
-			self.area = self.width * self.height
-			self.h = self.Nu * self.cond / s
-			self.QDot = self.area * self.h * self.deltaT
+		elif (self.geomConf == 'HC'):
+			fPr = (1 + (0.559 / self.Pr) ** (9.0 / 16)) ** (-16.0 / 9)
+			self.Nu = (0.6 + 0.387 * (self.Ra * fPr) ** (1.0 / 6)) ** 2			
 		else: 
 			pass
 		
+		# Compute the convection coefficient and the total heat flow rate
+		self.h = self.Nu * self.cond / s
+		self.QDot = self.area * self.h * self.deltaT
 		
 		
 		
