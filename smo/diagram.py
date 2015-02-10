a5bc0512b3161872cfcbe2252ca02e71f5b170f2
Ivaylo Mihaylov <ivombg@yahoo.com>
2015-02-10 19:11:57 +0200

    workk
    Signed-off-by: Ivaylo Mihaylov <ivombg@yahoo.com>

--- smo/diagram.py
+++ smo/diagram.py
@@ -1,28 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from smo.media.CoolProp.CoolProp import FluidState
+from smo.media.CoolProp.CoolProp import FluidState, Fluid
 
-fluidName = "ParaHydrogen" 
-fState = FluidState(fluidName)
-fState.update('P', 80*1e5, 'D', 1)
+fluidName = "CarbonDioxide" 
 fState = FluidState(fluidName)
 
-s_Arr = np.arange(1e3, 8e4, 1e3)
-p_Arr = np.arange(1e5, 80 * 1e5, 10 * 1e5)
+f = Fluid(fluidName)
+# f.critical['p']
 
-# fig = plt.figure()
-# ax = plt.gca()
+s_Arr = np.arange(900, 2200, 10)
+p_Arr = np.arange(40 * 1e5, 120 * 1e5, 10 * 1e5)
+
+fig = plt.figure()
+ax1 = plt.gca()
 
 for p in p_Arr:
     T_Arr = np.zeros((len(s_Arr),))
+#     vQ_Arr = np.zeros((len(s_Arr),))
     for i in range(len(s_Arr)):
         fState.update('S', s_Arr[i], 'P', p)
-        T_Arr[i] = fState.T
-    plt.plot(s_Arr, T_Arr, label="p=" + str(p/1e5) + ' bar')
+        T_Arr[i] = fState.T - 273.15
+#         if fState.isTwoPhase():
+#             fState.update_Tq('S', s_Arr[i], 'Q', p)
+        
+    ax1.plot(s_Arr, T_Arr, label = "p=" + str(p/1e5) + " bar", color = 'b')
 
+ax1.set_ylim([0, 100])
 plt.title(fluidName)
-plt.xlabel('S [kJ/kgK]')
-plt.ylabel('T [K]')
+plt.xlabel('S [J/kgK]')
+plt.ylabel('T [C]')
 plt.legend()
 plt.show()
         
