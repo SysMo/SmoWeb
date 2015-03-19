64651f67bf4d0f401d92b669c7f65b2d4e3b2eff
milen <milen_kb@abv.bg>
2015-03-19 18:31:51 +0200

    Changed in SimpleChemostat.
    Signed-off-by: milen <milen_kb@abv.bg>

--- BioReactors/models/SimpleChemostatModel.py
+++ BioReactors/models/SimpleChemostatModel.py
@@ -11,18 +11,18 @@ class SimpleChemostatModel(NumericalModel):
     
     #1. ############ Inputs ###############
     #1.1 Fields - Input values
-    S_in = Quantity('Density', default = (5, 'g/L'), minValue = 0, label ='S<sub>in</sub>', description = 'concentration of the input substrate [mass/volume]')
-    m = Quantity('Hertz', default = (3, '1/h'), minValue = 0, label = 'm', description = 'maximal growth rate [1/time]')
-    K = Quantity('Density', default = (3.7, 'g/L'), minValue = 0, label = 'K', description = 'half saturation constant [mass/volume]')
-    gamma = Quantity(default = 0.6, minValue = 0, maxValue = 1.0, label = '&#947', description = 'yield coefficient of microorganisms [-]')
+    S_in = Quantity('Density', default = (5, 'g/L'), minValue = 0, label ='S<sub>in</sub>', description = 'concentration of the input substrate')
+    m = Quantity('Hertz', default = (3, '1/h'), minValue = 0, label = 'm', description = 'maximal growth rate')
+    K = Quantity('Density', default = (3.7, 'g/L'), minValue = 0, label = 'K', description = 'half saturation constant')
+    gamma = Quantity(default = 0.6, minValue = 0, maxValue = 1.0, label = '&#947', description = 'yield coefficient of microorganisms')
     D_vals = RecordArray((
             ('time', Quantity('Time', default = (20, 'h'), minValue = 0, label = 'Duration')),
             ('D', Quantity('Hertz', default = (1, '1/h'), minValue = 0, label = 'D')),
         ), label = 'D', description = 'dilution rate [1/time]')  
     parametersFieldGroup = FieldGroup([S_in, m, K, gamma, D_vals], label = "Parameters")
     
-    S0 = Quantity('Density', default = (0, 'g/L'), minValue = 0, label = 'S<sub>0</sub>', description = 'initial concentration of substrate [mass/volume]')
-    X0 = Quantity('Density', default = (0.5, 'g/L'), minValue = 0, label = 'X<sub>0</sub>', description = 'initial concentration of microorganisms [mass/volume]')
+    S0 = Quantity('Density', default = (0, 'g/L'), minValue = 0, label = 'S<sub>0</sub>', description = 'initial concentration of substrate')
+    X0 = Quantity('Density', default = (0.5, 'g/L'), minValue = 0, label = 'X<sub>0</sub>', description = 'initial concentration of microorganisms')
     initialValuesFieldGroup = FieldGroup([S0, X0], label = "Initial values")
     
     inputValuesSuperGroup = SuperGroup([parametersFieldGroup, initialValuesFieldGroup], label = "Input values")
