7e9f161633b996e334af4d913d0d3156746ec186
Ivaylo Mihaylov <ivombg@yahoo.com>
2015-04-16 16:19:17 +0300

    temp
    Signed-off-by: Ivaylo Mihaylov <ivombg@yahoo.com>

--- ThermoFluids/models/heat_exchangers/HeatExchangerCrossSectionProfile.py
+++ ThermoFluids/models/heat_exchangers/HeatExchangerCrossSectionProfile.py
@@ -12,12 +12,16 @@ class HeatExchangerCrossSectionProfile():
         self.dimensionLabels = []
         self.dimensionLabelAngles = []
     
+    def addDimension(self, line, label, labelAngle):
+        self.dimensionLines.append(line)
+        self.dimensionLabels.append(label)
+        self.dimensionLabelAngles.append(labelAngle)
+    
     def addBlock(self, blockGeom):
         self.blockRadius = blockGeom.diameter / 2.
         self.patches.append(Circle((self.originX, self.originY), self.blockRadius))
-        self.dimensionLines.append([[self.originX, self.originX-self.blockRadius], [self.originY, self.originY]])
-        self.dimensionLabels.append(self.blockRadius)
-        self.dimensionLabelAngles.append(0)
+        self.addDimension([[self.originX, self.originX-self.blockRadius], [self.originY, self.originY]],
+                          self.blockRadius, 0)
     
     def addChannels(self, channelsGeom):
         for i in range(channelsGeom.number):
@@ -29,33 +33,43 @@ class HeatExchangerCrossSectionProfile():
             channelRadius = channelsGeom.externalDiameter / 2.
             self.patches.append(Circle((channelCenterX, channelCenterY), channelRadius))
             if (i == 0):
-                self.dimensionLines.append([[self.originX, channelCenterX], [self.originY, channelCenterY]])
-                self.dimensionLabels.append(channelsGeom.radialPosition)
-                self.dimensionLabelAngles.append(angle)
-                self.dimensionLines.append([[channelCenterX + channelRadius * math.cos(math.radians(angle + 90)), channelCenterX + channelRadius * math.cos(math.radians(angle - 90))], 
-                                            [channelCenterY + channelRadius * math.sin(math.radians(angle + 90)) , channelCenterY + channelRadius * math.sin(math.radians(angle - 90))]])
-                self.dimensionLabels.append(channelsGeom.externalDiameter)
-                self.dimensionLabelAngles.append(angle-90)
+                self.addDimension([[self.originX, channelCenterX], [self.originY, channelCenterY]],
+                                  channelsGeom.radialPosition, angle)
+                self.addDimension([[channelCenterX + channelRadius * math.cos(math.radians(angle + 90)), channelCenterX + channelRadius * math.cos(math.radians(angle - 90))], [channelCenterY + channelRadius * math.sin(math.radians(angle + 90)) , channelCenterY + channelRadius * math.sin(math.radians(angle - 90))]],
+                                  channelsGeom.externalDiameter, angle-90)
     
     def plotGeometry(self, ax = None):
         if (ax is None):
             fig = plt.figure()
             ax = fig.add_subplot(111)
         ax.set_aspect(1)   
-        pc = PatchCollection(self.patches, match_original = True, autolim = True)
+        pc = PatchCollection(self.patches, match_original = True)
         pc.set_facecolor('w')
         pc.set_edgecolor('k')
-        ax.add_collection(pc)     
+        ax.add_collection(pc)    
         self.plotDimensions(ax)
         ax.set_xlim(-1.1 * self.blockRadius, 1.1 * self.blockRadius)
         ax.set_ylim(-1.1 * self.blockRadius, 1.1 * self.blockRadius)
         plt.show()
     
+    
     def plotDimensions(self, ax):
         for i in range(len(self.dimensionLines)):
-            plt.plot(self.dimensionLines[i][0], self.dimensionLines[i][1])
+            x1, x2 = self.dimensionLines[i][0]
+            y1, y2 = self.dimensionLines[i][1]
+            plt.plot([x1, x2], [y1, y2])
+            
+            if (x1 == x2):
+                xLabel = x1
+                yLabel = y1 + (y2 - y1) / 2.
+            else:
+                a = (y2 - y1) / (x2 - x1)      
+                b = y1 - a * x1
+                xLabel = x1 + (x2 - x1) / 2.
+                yLabel = a * xLabel + b
             ax.annotate("{0}".format(self.dimensionLabels[i] * 1e3), 
-                                    xy = (self.dimensionLines[i][0][1], self.dimensionLines[i][1][1]),
+                                    xy = (xLabel, yLabel),
+#                                     xy = (self.dimensionLines[i][0][1], self.dimensionLines[i][1][1]),
                                     rotation = self.dimensionLabelAngles[i])
         
     @staticmethod
