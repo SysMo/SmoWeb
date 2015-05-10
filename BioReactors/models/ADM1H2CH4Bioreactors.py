58b645addca445351c0ef8d2ea234a9d13d50df4
milen <milen_kb@abv.bg>
2015-05-10 22:16:40 +0300

    ADM1 - Added ADM1CH4Bioreactor
    Signed-off-by: milen <milen_kb@abv.bg>

--- BioReactors/models/ADM1H2CH4Bioreactors.py
+++ BioReactors/models/ADM1H2CH4Bioreactors.py
@@ -8,6 +8,7 @@ import numpy as np
 import smo.model.fields as F
 import smo.model.actions as A 
 import smo.dynamical_models.bioreactors.ADM1H2Bioreactor as DM
+import smo.dynamical_models.bioreactors.ADM1CH4Bioreactor as DM1
 
 from smo.model.model import NumericalModel
 #from smo.web.modules import RestModule
@@ -18,6 +19,144 @@ class SolverSettings(NumericalModel):
     
     FG = F.FieldGroup([tFinal, tPrint], label = 'Solver')
     SG = F.SuperGroup([FG], label = 'Settings')
+    
+    modelBlocks = []
+    
+class CH4Bioreactor(NumericalModel):
+    # Stoichiometric parameter values
+    Y_ac = F.Quantity(default = 0.0, minValue = 0.0,
+        label = 'Y<sub>su</sub>', description = 'yield of sugar degraders on acetate')
+    
+    stoichiometricParametersFG = F.FieldGroup([
+            Y_ac,
+        ],
+        label = 'Stoichiometric parameters (R-CH4)'
+    )
+    
+    # Biochemical parameter values
+    k_m_ac = F.Quantity('Bio_TimeRate', default = (0.0, '1/day'), minValue = (0, '1/day'), 
+        label = 'k<sub>m,ac</sub>', description = 'specific Monod maximum uptake rate of acetate degraders')
+    K_S_ac = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'K<sub>S,ac</sub>', description = 'Monod half saturation constant of acetate degraders')
+  
+    
+    biochemicalParametersFG = F.FieldGroup([
+            k_m_ac, K_S_ac, 
+        ],
+        label = 'Biochemical parameters (R-CH4)'
+    )
+    
+    # Physiochemical parameters (Temperatures)
+    T_base = F.Quantity('Temperature', default = (0.0, 'degC'),
+        label = 'T<sub>base</sub>', description = 'base (ambient) temperature')
+    T_op = F.Quantity('Temperature', default = (0.0, 'degC'),
+        label = 'T<sub>op</sub>', description = 'operating temperature')
+    
+    temperatureParametersFG = F.FieldGroup([
+            T_base, T_op,
+        ],
+        label = 'Temperatures (R-CH4)'
+    )
+    
+    # Physiochemical parameters
+    kLa_ch4 = F.Quantity('Bio_TimeRate', default = (0.0, '1/day'), minValue = (0, '1/day'), 
+        label = 'k<sub>L</sub>a<sub> ch4</sub>', description = 'gas-liquid transfer coefficient of methane')
+
+    physiochemicalParametersFG = F.FieldGroup([
+           kLa_ch4,
+        ],
+        label = 'Physiochemical parameters (R-CH4)'
+    )
+
+    # Physical parameters
+    V_liq = F.Quantity('Volume', default = (0.0, 'L'),
+        label = 'V<sub>liq</sub>', description = 'volume of the liquid part of the reactor')
+    V_gas = F.Quantity('Volume', default = (0.0, 'L'),
+        label = 'V<sub>gas</sub>', description = 'volume of the gas headspace of the reactor')
+
+    physicalParametersFG = F.FieldGroup([
+            V_liq, V_gas,
+        ],
+        label = 'Physical parameters (R-CH4)'
+    )
+    
+    # Volumetri flow rates
+    q_liq_vals = F.RecordArray(
+        (
+            ('time', F.Quantity('Bio_Time', default = (10.0, 'day'), minValue = (0, 'day'), label = 'Duration')),
+            ('q', F.Quantity('Bio_VolumetricFlowRate', default = (0.17, 'L/day'), minValue = (0, 'L/day'), label = 'Flow rate')),
+        ), 
+        label = 'q<sub> liq</sub>', 
+        description = 'liquid flow rate (dilution/wash-out)',
+        toggle = True,
+    ) 
+    
+    q_gas = F.Quantity('Bio_VolumetricFlowRate', default = (0.0, 'L/day'), minValue = (0, 'L/day'), 
+        label = 'q<sub> gas</sub>', description = 'gas flow rate (wash-out)')
+
+    volumetricFlowRatesFG = F.FieldGroup([
+            q_liq_vals, q_gas
+        ],
+        label = 'Volumetric flow rates (R-CH4)' #'Controller
+    )
+    
+    # Input concentrations of components
+    S_ac_in = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'S<sub>ac</sub><sup> (in)</sup>', description = 'input concentration of acetate')
+    
+    S_ch4_in = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'S<sub>ch4</sub><sup> (in)</sup>', description = 'input concentration of methane')
+
+    X_ac_in = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'X<sub>ac</sub><sup> (in)</sup>', description = 'input concentration of acetate degraders')
+
+    inputConcentrationsFG = F.FieldGroup([
+            S_ac_in, S_ch4_in,
+            X_ac_in, 
+        ],
+        label = 'Input concentrations (R-CH4)'
+    )
+    
+    # Initial concentrations of components
+    S_ac_from_RH2 = F.Boolean(True, 
+        label = 'S<sub>ac</sub> <sup>(0)</sup> = S<sub>ac</sub><sup>(R-H2)</sup>', description = 'take the inital acetate concentration from the H2 Bioreactor (R-H2)')
+    
+    S_ac_0 = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'S<sub>ac</sub><sup> (0)</sup>', description = 'initial concentration of acetate') 
+        #show = 'self.concentrationsRCH4.S_ac_from_RH2 == False')
+
+    S_ch4_0 = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'S<sub>ch4</sub><sup> (0)</sup>', description = 'initial concentration of methane (liquid)')
+    
+    X_ac_0 = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'X<sub>ac</sub><sup> (0)</sup>', description = 'initial concentration of acetate degraders')
+   
+    S_gas_ch4_0 = F.Quantity('Bio_CODConcentration', default = (0.0, 'kgCOD/m**3'), minValue = (0, 'kgCOD/m**3'), 
+        label = 'S<sub>gas,ch4</sub><sup> (0)</sup>', description = 'initial concentration of methane (gas)')
+
+    initialConcentrationsFG = F.FieldGroup([
+            S_ac_from_RH2, 
+            S_ac_0, S_ch4_0,
+            X_ac_0,
+            S_gas_ch4_0,
+        ],
+        label = 'Initial concentrations (R-CH4)'
+    )
+    
+    #1.2 Super groups - parameters & concentrations   
+    parametersSG = F.SuperGroup([
+            stoichiometricParametersFG, biochemicalParametersFG,
+            temperatureParametersFG, physiochemicalParametersFG, 
+            physicalParametersFG, volumetricFlowRatesFG,
+        ], 
+        label = "Parameters (R-CH4)"
+    )
+    
+    concentrationsSG = F.SuperGroup([
+            initialConcentrationsFG, inputConcentrationsFG,
+        ], 
+        label = "Concentrations (R-CH4)"
+    )
     
     modelBlocks = []
     
@@ -263,6 +402,9 @@ class ADM1H2CH4Bioreactors(NumericalModel):
     parametersRH2 = F.SubModelGroup(H2Bioreactor, 'parametersSG', label = 'Parameters (R-H2)')
     concentrationsRH2 = F.SubModelGroup(H2Bioreactor, 'concentrationsSG', label = 'Concentrations (R-H2)')
     
+    parametersRCH4 = F.SubModelGroup(CH4Bioreactor, 'parametersSG', label = 'Parameters (R-CH4)')
+    concentrationsRCH4 = F.SubModelGroup(CH4Bioreactor, 'concentrationsSG', label = 'Concentrations (R-CH4)')
+    
     #1.3 Fields - Settings
     solverSettingsRH2 = F.SubModelGroup(SolverSettings, 'FG', label = 'Bioreactor (R-H2)')
     solverSettingsRCH4 = F.SubModelGroup(SolverSettings, 'FG', label = 'Bioreactor (R-CH4)')
@@ -279,13 +421,13 @@ class ADM1H2CH4Bioreactors(NumericalModel):
      
     inputView = F.ModelView(
         ioType = "input", 
-        superGroups = [parametersRH2, concentrationsRH2, settingsSG], 
+        superGroups = [parametersRH2, concentrationsRH2, parametersRCH4, concentrationsRCH4, settingsSG], 
         autoFetch = True,
         actionBar = A.ActionBar([exampleAction]),
     )
     
-    #2. ############ Results ###############
-    varTuples = (
+    #2. ############ Results (R-H2) ###############
+    varTuplesRH2 = (
         ('time', F.Quantity('Bio_Time', default=(1, 'day'))),
         ('S_su', F.Quantity('Bio_CODConcentration', default=(1, 'kgCOD/m**3'))),
         ('S_aa', F.Quantity('Bio_CODConcentration', default=(1, 'kgCOD/m**3'))),
@@ -304,22 +446,49 @@ class ADM1H2CH4Bioreactors(NumericalModel):
         ('D', F.Quantity('Bio_TimeRate', default=(1, '1/day'))),
     )
     
-    plot = F.PlotView(
-        varTuples,
+    plotRH2 = F.PlotView(
+        varTuplesRH2,
         label='Plot', 
         options = {'ylabel' : None}, 
+        visibleColumns = [0, 4, 6, 13]
     )
-    table = F.TableView(
-        varTuples,
+    tableRH2 = F.TableView(
+        varTuplesRH2,
         label='Table', 
-        options = {'title': 'Simple Chemostat', 'formats': ['0.000', '0.000', '0.000', '0.000']}
+        options = {'title': 'H2 Bioreactor', 'formats': ['0.000', '0.000', '0.000', '0.000']}
     )
     
-    resultsVG = F.ViewGroup([plot, table], label = 'Results')
-    resultsSG = F.SuperGroup([resultsVG])
+    resultsRH2VG = F.ViewGroup([plotRH2, tableRH2])
+    resultsRH2SG = F.SuperGroup([resultsRH2VG], label = 'Results (R-H2)')
+    
+    #2. ############ Results (R-CH4) ###############
+    varTuplesRCH4 = (
+        ('time', F.Quantity('Bio_Time', default=(1, 'day'))),
+        ('S_ac', F.Quantity('Bio_CODConcentration', default=(1, 'kgCOD/m**3'))),
+        ('S_ch4', F.Quantity('Bio_CODConcentration', default=(1, 'kgCOD/m**3'))),
+        ('X_ac', F.Quantity('Bio_CODConcentration', default=(1, 'kgCOD/m**3'))),
+        ('S_gas_ch4', F.Quantity('Bio_CODConcentration', default=(1, 'kgCOD/m**3'))),
+        ('m_gas_ch4', F.Quantity('Bio_Mass', default=(1, 'kgCOD'))),
+        ('D', F.Quantity('Bio_TimeRate', default=(1, '1/day'))),
+    )
+    
+    plotRCH4 = F.PlotView(
+        varTuplesRCH4,
+        label='Plot', 
+        options = {'ylabel' : None},
+        visibleColumns = [0, 1, 2, 4]
+    )
+    tableRCH4 = F.TableView(
+        varTuplesRCH4,
+        label='Table', 
+        options = {'title': 'CH4 Bioreactor', 'formats': ['0.000', '0.000', '0.000', '0.000']}
+    )
+    
+    resultsRCH4VG = F.ViewGroup([plotRCH4, tableRCH4])
+    resultsRCH4SG = F.SuperGroup([resultsRCH4VG], label = 'Results (R-CH4)')
     
     #2.1 Model view
-    resultView = F.ModelView(ioType = "output", superGroups = [resultsSG])
+    resultView = F.ModelView(ioType = "output", superGroups = [resultsRH2SG, resultsRCH4SG])
     
     ############# Page structure ########
     modelBlocks = [inputView, resultView]
@@ -329,6 +498,7 @@ class ADM1H2CH4Bioreactors(NumericalModel):
         self.exampleDef()
         
     def exampleDef(self):
+        ############# H2 Bioreactor ###############
         #Stoichiometric parameter values
         self.parametersRH2.f_ch_xc = 0.2 #-
         self.parametersRH2.f_pr_xc = 0.2 #-
@@ -413,15 +583,70 @@ class ADM1H2CH4Bioreactors(NumericalModel):
         # Solver settings
         self.solverSettingsRH2.tFinal = (50.0, 'day')
         self.solverSettingsRH2.tPrint = (0.025, 'day')
-                        
-    def compute(self):
-        bioreactor = DM.ADM1H2Bioreactor(self.parametersRH2, self.concentrationsRH2)
         
-        bioreactor.prepareSimulation()
-        bioreactor.run(self.solverSettingsRH2)
         
-        res = bioreactor.getResults()
-        results = np.array(res)
-        self.plot = results
-        self.table = results
+        ############# CH4 Bioreactor ###############
+        #Stoichiometric parameter values
+        self.parametersRCH4.Y_ac = 0.05 #-
+        
+        #Biochemical parameter values
+        self.parametersRCH4.k_m_ac = (8.0, '1/day')
+        self.parametersRCH4.K_S_ac = (0.15, 'kgCOD/m**3') 
+
+        # Physiochemical parameter values (Temperatures)
+        self.parametersRCH4.T_base = (15.0, 'degC')
+        self.parametersRCH4.T_op = (35.0, 'degC')
     
+        # Physiochemical parameter values
+        self.parametersRCH4.kLa_ch4 = (200, '1/day')
+        
+        # Physical parameter valures
+        self.parametersRCH4.V_liq = (34.0, 'L')
+        self.parametersRCH4.V_gas = ( 3.0, 'L')
+        
+        # Volumetric flow rate values
+        self.parametersRCH4.q_liq_vals[0] = (10, 1.7*1e-3) #(day, m**3/day)
+        self.parametersRCH4.q_gas = (30.0, 'L/day')
+        
+        # Input concentrations 
+        self.concentrationsRCH4.S_ac_in = (0.2, 'kgCOD/m**3')
+        self.concentrationsRCH4.S_ch4_in = (0 * 1e-5, 'kgCOD/m**3')
+        self.concentrationsRCH4.X_ac_in = (0 * 0.01, 'kgCOD/m**3')
+        
+        # Initial values of state variables 
+        self.concentrationsRCH4.S_ac_0 = (0.2, 'kgCOD/m**3')
+        self.concentrationsRCH4.S_ch4_0 = (0.055, 'kgCOD/m**3')
+        self.concentrationsRCH4.X_ac_0 = (0.76, 'kgCOD/m**3')
+        self.concentrationsRCH4.S_gas_ch4_0 = (1e-5, 'kgCOD/m**3')
+        
+        # Solver settings
+        self.solverSettingsRCH4.tFinal = (50.0, 'day')
+        self.solverSettingsRCH4.tPrint = (0.025, 'day')
+        
+                    
+    def compute(self):
+        # Simulate H2 Bioreactor
+        bioreactorRH2 = DM.ADM1H2Bioreactor(self.parametersRH2, self.concentrationsRH2)
+        
+        bioreactorRH2.prepareSimulation()
+        bioreactorRH2.run(self.solverSettingsRH2)
+        
+        resRH2 = bioreactorRH2.getResults()
+        resultsRH2 = np.array(resRH2)
+        self.plotRH2 = resultsRH2
+        self.tableRH2 = resultsRH2
+        
+        # Connnect the two bioreactors
+        if self.concentrationsRCH4.S_ac_from_RH2:
+            self.concentrationsRCH4.S_ac_0 = resultsRH2['S_ac'][-1]
+    
+        # Simulate CH4 Bioreactor
+        bioreactorRCH4 = DM1.ADM1CH4Bioreactor(self.parametersRCH4, self.concentrationsRCH4)
+        
+        bioreactorRCH4.prepareSimulation()
+        bioreactorRCH4.run(self.solverSettingsRCH4)
+        
+        resRCH4 = bioreactorRCH4.getResults()
+        resultsRCH4 = np.array(resRCH4)
+        self.plotRCH4 = resultsRCH4
+        self.tableRCH4 = resultsRCH4
---
+++ smo/dynamical_models/bioreactors/ADM1CH4Bioreactor.py
@@ -0,0 +1,273 @@
+'''
+Created on Mar 08, 2015
+
+@author: Milen Borisov
+@copyright: SysMo Ltd, Bulgaria
+'''
+import numpy as np
+import pylab as plt
+import Constnat as Const
+
+from smo.util import AttributeDict 
+from assimulo.exception import TerminateSimulation
+from smo.dynamical_models.core.Simulation import Simulation, TimeEvent, ResultStorage
+from smo.math.util import NamedStateVector
+
+""" Global Settings """
+import os
+from SmoWeb.settings import MEDIA_ROOT
+tmpFolderPath = os.path.join (MEDIA_ROOT, 'tmp')
+csvFileName = os.path.join(tmpFolderPath, 'BioReactors_ADM1CH4Bioreactor_SimulationResults.csv')
+dataStorageFilePath =  os.path.join(tmpFolderPath, 'BioReactors_SimulationResults.h5')
+dataStorageDatasetPath = '/ADM1CH4Bioreactor'
+
+class ADM1TimeEvent(TimeEvent):	
+	"""
+	Class for time event of ADM1 models
+	"""
+	def __init__(self, t, newValue_D):
+		self.t = t
+		self.newValue_D = newValue_D
+		
+		self.eventType = "ADM1_TIME_EVENT"
+		self.description = "Change the dilution rate (D) to {0}".format(newValue_D)
+		
+class ADM1CH4Bioreactor(Simulation):
+	"""
+	Class for implementation the model of a ADM1 CH4Bioreactor
+	"""
+	name = 'Model of a bioreactor that produces hydrogen.'
+	
+	def __init__(self, params, concentrs, **kwargs):
+		super(ADM1CH4Bioreactor, self).__init__(**kwargs)
+					
+		# Initialize parameters		
+		self.params = params
+		self.K_H_ch4 = 0.0014 * np.exp(
+			-14240./(Const.R*100.) * (1/params.T_base - 1/params.T_op))
+		
+		self.D_gas = params.q_gas/params.V_gas
+		#:TRICKY: compute D values using D = q/V
+		self.D_liq_vals = np.copy(params.q_liq_vals)
+		for D_liq in self.D_liq_vals:
+			q_liq = D_liq[1]
+			D_liq[1] = q_liq / params.V_liq
+			
+		# Initialize concentrations
+		self.concentrs = concentrs
+		
+		# Create state vector and derivative vector
+		stateVarNames = [
+			'S_ac', 'S_ch4',
+			'X_ac',  
+			'S_gas_ch4', 'm_gas_ch4',
+		] 
+		self.y = NamedStateVector(stateVarNames)
+		self.yRes = NamedStateVector(stateVarNames)
+		self.yDot = NamedStateVector(stateVarNames)
+
+		# Initialize data storage
+		self.resultStorage = ResultStorage(
+			filePath = dataStorageFilePath,
+			datasetPath = dataStorageDatasetPath)
+		if (kwargs.get('initDataStorage', True)):
+			self.resultStorage.initializeWriting(
+				varList = ['t'] + stateVarNames + ['D'],
+				chunkSize = 1e4)
+		
+		# Register time event (changed of D)
+		D_liq = self.D_liq_vals[0]
+		self.D = D_liq[1]
+		tChangedD = D_liq[0]
+		
+		for i in range(len(self.D_liq_vals)-1):
+			D_liq = self.D_liq_vals[i+1]
+			self.timeEventRegistry.add(ADM1TimeEvent(t = tChangedD, newValue_D = D_liq[1]))
+			tChangedD += D_liq[0]
+		
+		# Set initial values of the states
+		self.y.S_ac = concentrs.S_ac_0
+		self.y.S_ch4 = concentrs.S_ch4_0
+		self.y.X_ac = concentrs.X_ac_0
+		self.y.S_gas_ch4 = concentrs.S_gas_ch4_0
+		self.y.m_gas_ch4 = 0.0		
+				
+		# Set all the initial state values
+		self.y0 = self.y.get(copy = True)
+		
+		# Set the initial flags
+		self.sw0 = [True]
+		
+	def rhs(self, t, y, sw):
+		params = self.params
+		concentrs = self.concentrs
+		
+		# Set state values
+		self.y.set(y)
+		
+		try:
+			# Get state values
+			S_ac = self.y.S_ac
+			S_ch4 = self.y.S_ch4
+			X_ac = self.y.X_ac
+			S_gas_ch4 = self.y.S_gas_ch4
+			
+			# Compute biochemical process rates
+			r11 = params.k_m_ac * (S_ac / (params.K_S_ac + S_ac)) * X_ac
+			
+			# Compute transfer rates
+			p_gas_ch4 = S_gas_ch4 * (Const.R * params.T_op)/64.
+			r_T_9 = params.kLa_ch4 * (S_ch4 - 64 * self.K_H_ch4 * p_gas_ch4)
+			
+			# Compute state derivatives		
+			S_ac_dot = self.D*(concentrs.S_ac_in - S_ac) -r11#7.2
+			S_ch4_dot = self.D*(concentrs.S_ch4_in - S_ch4) + \
+				(1-params.Y_ac)*r11 - r_T_9 #9.2
+			X_ac_dot = self.D*(concentrs.X_ac_in - X_ac) + params.Y_ac * r11 #22.2
+			S_gas_ch4_dot = self.D_gas*(0. - S_gas_ch4) + r_T_9 * params.V_liq / params.V_gas #1.2
+			m_gas_ch4_dot = params.q_gas * S_gas_ch4
+			
+		except Exception, e:
+			self.resultStorage.finalizeResult()
+			# Log the error if it happens in the rhs() function
+			print("Exception at time {}: {}".format(t, e))
+			raise e
+			
+		self.yDot.S_ac = S_ac_dot
+		self.yDot.S_ch4 = S_ch4_dot
+		self.yDot.X_ac = X_ac_dot
+		self.yDot.S_gas_ch4 = S_gas_ch4_dot
+		self.yDot.m_gas_ch4 = m_gas_ch4_dot
+		return self.yDot.get()
+	
+	
+	def state_events(self, t, y, sw):
+		eventIndicators = np.ones(len(sw))
+		return eventIndicators
+	
+	def step_events(self, solver):
+		# Called on each time step
+		pass
+	
+	def handle_event(self, solver, eventInfo):
+		reportEvents = True
+		_stateEventInfo, timeEvent = eventInfo
+		
+		# Handle time events
+		if (timeEvent):
+			timeEventList = self.processTimeEvent(solver.t)
+			self.D = timeEventList[0].newValue_D
+			if (reportEvents):
+				print("Time event located at time: {} - {}".format(solver.t, timeEventList[0].description))
+	
+		if (False):
+			raise TerminateSimulation()
+	
+	def handle_result(self, solver, t, y):
+		super(ADM1CH4Bioreactor, self).handle_result(solver, t, y)
+			
+		self.yRes.set(y)
+		self.resultStorage.record[:] = (t, 
+			self.yRes.S_ac, self.yRes.S_ch4,  
+			self.yRes.X_ac, 
+			self.yRes.S_gas_ch4, self.yRes.m_gas_ch4,
+			self.D,
+		)
+		self.resultStorage.saveTimeStep()
+		
+	def getResults(self):
+		return self.resultStorage.data
+	
+	def loadResult(self, simIndex):
+		self.resultStorage.loadResult(simIndex)
+	
+	def plotHDFResults(self):		
+		data = self.resultStorage.data
+		xData = data['t']
+		plt.plot(xData, data['S_ac'], 'r', label = 'S_ac')
+		plt.plot(xData, data['S_ch4'], 'b', label = 'S_ch4')
+		plt.plot(xData, data['X_ac'], 'g', label = 'X_ac')
+		plt.plot(xData, data['S_gas_ch4'], 'g--', label = 'S_gas_ch4')
+		plt.plot(xData, data['D'], 'm', label = 'D')
+		
+		plt.gca().set_xlim([0, xData[-1]])
+		plt.legend()
+		plt.show()
+
+
+def TestADM1CH4Bioreactor():
+	print "=== BEGIN: TestADM1CH4Bioreactor ==="
+	
+	# Settings
+	simulate = True #True - run simulation; False - plot an old results 
+	
+	# Initialize simulation parameters
+	solverParams = AttributeDict({
+		'tFinal' : 10., 
+		'tPrint' : .1,
+	})
+		
+	# Initialize model parameters
+	# Validation of prameters
+	#f_ch_xc + f_pr_xc + f_li_xc + [f_xI_xc + f_sI_xc] = 1.0
+	#[f_bu_su + f_pro_su] + f_ac_su + f_h2_su = 1.0
+	#[f_va_aa + f_bu_aa + f_pro_aa] + f_ac_aa + f_h2_aa = 1.0
+	#Y_xx < = 1.0
+	class ModelParams:
+		#Stoichiometric parameter values
+		Y_ac = 0.05 #-
+		
+		#Biochemical parameter values
+		k_m_ac = 8.0 #1/day
+		K_S_ac = 0.15 #kgCOD/m**3
+		
+		# Physiochemical parameter values
+		T_base = 298.15 #K
+		T_op = 308.15 #K
+	
+		kLa_ch4 = 200 #1/day
+		
+		# Physical parameters
+		V_liq = 3.4 #L
+		V_gas = 0.3 #L
+		
+		# Controller - D = q/V
+		q_liq_vals = np.array([[100, 0.17], ]) #[day, L/day] (liquid)
+		q_gas = 3.0 #L/day
+	modelParams = ModelParams()		
+		
+	class ModelConcentrs:
+		# Input concentrations 
+		S_ac_in = 0.2 #kgCOD/m**3
+		S_ch4_in = 1e-5 #kgCOD/m**3
+		X_ac_in = 0 * 0.01 #kgCOD/m**3
+		
+		# Initial values of state variables 
+		S_ac_0 = 0.2 #kgCOD/m**3
+		S_ch4_0 = 0.055 #kgCOD/m**3
+		X_ac_0 = 0.76 #kgCOD/m**3
+		S_gas_ch4_0 = 1e-5 #kgCOD/m**3
+	modelConcentrs = ModelConcentrs()
+
+	
+	# Create the model
+	bioreactor = ADM1CH4Bioreactor(params = modelParams, concentrs = modelConcentrs, initDataStorage = simulate)
+	
+	# Run simulation or load old results
+	if (simulate == True):
+		bioreactor.prepareSimulation()
+		bioreactor.run(solverParams)
+	else:
+		bioreactor.loadResult(simIndex = 1)
+	
+	# Export to csv file
+	bioreactor.resultStorage.exportToCsv(fileName = csvFileName)
+	
+	# Plot results
+	bioreactor.plotHDFResults()
+	
+	print "=== END: TestADM1CH4Bioreactor ==="
+	
+	
+if __name__ == '__main__':
+	TestADM1CH4Bioreactor()
